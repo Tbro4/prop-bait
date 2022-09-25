7a57c52888d21ddc1e45d61d2dba9f11b66b43c2
Tyler Brooks <thetylerbrooks@gmail.com>
2022-09-24 21:01:52 -0600

    Able to add user to db via Signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,64 @@
 import "./App.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Account from "./pages/Account";
 import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 import AppBarTop from "./components/AppBarTop/AppBarTop";
 import AppBarBottom from "./components/AppBarBottom/AppBarBottom";
-
 import Cart from "./pages/Cart";
 import { Container } from "@mui/system";
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <Router>
-      <Container maxWidth="xs">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Shop" element={<Shop />} />
-          <Route path="/Account" element={<Account />} />
-          <Route path="/Cart" element={<Cart />} />
-        </Routes>
-        <AppBarTop />
-        <AppBarBottom />
-      </Container>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Container maxWidth="xs">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Shop" element={<Shop />} />
+            <Route path="/Account" element={<Account />} />
+            <Route path="/Cart" element={<Cart />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Signup" element={<Signup />} />
+          </Routes>
+          <AppBarTop />
+          <AppBarBottom />
+        </Container>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,15 +1,26 @@
 //render either their account page or Login/Signup page if they arent logged in
 import React from "react";
+import { Button, Container, Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Account = () => {
   return (
-    <div className="App">
+    <>
       <br></br>
       <br></br>
       <br></br>
       <br></br>
-      <h1>Account Page</h1>
-    </div>
+      <Container>
+        <Stack spacing={2}>
+          <Button variant="contained" component={Link} to="/Login">
+            Login
+          </Button>
+          <Button variant="contained" component={Link} to="/Signup">
+            Signup
+          </Button>
+        </Stack>
+      </Container>
+    </>
   );
 };
 
