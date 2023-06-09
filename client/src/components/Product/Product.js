4b803fb171c0b4dd5b65c19b6bf631246fe16ce8
Tyler Brooks <thetylerbrooks@gmail.com>
2023-06-08 19:22:37 -0600

    raw product data is on page, needs to be manipulated and styled

--- client/src/components/Product/Product.js
+++ client/src/components/Product/Product.js
@@ -24,17 +24,92 @@ const Product = ({ productId }) => {
     console.log("Product added to cart:", product);
   };
 
+  const renderMeasurements = () => {
+    if (product?.hasMeasurement) {
+      const measurements = Object.entries(product.hasMeasurement).filter(
+        ([key, value]) => value !== null
+      );
+
+      if (measurements.length > 0) {
+        return (
+          <div>
+            <h4>Measurements:</h4>
+            <table>
+              <tbody>
+                {measurements.map(([key, value]) => (
+                  <tr key={key}>
+                    <td>{key}</td>
+                    <td>{value}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        );
+      }
+    }
+    return null;
+  };
+
+  const renderOptions = () => {
+    if (product?.options && product.options.length > 0) {
+      return (
+        <div>
+          <h4>Options:</h4>
+          <table>
+            <thead>
+              <tr>
+                {product.options[0].image && <th>Image</th>}
+                {Object.keys(product.options[0]).map(
+                  (key) =>
+                    key !== "image" &&
+                    product.options[0][key] !== null && <th key={key}>{key}</th>
+                )}
+              </tr>
+            </thead>
+            <tbody>
+              {product.options.map((option, index) => (
+                <tr key={index}>
+                  {option.image && (
+                    <td>
+                      <img
+                        src={require(`../../images/${option.image}`)}
+                        alt={option.color}
+                      />
+                    </td>
+                  )}
+                  {Object.entries(option).map(
+                    ([key, value]) =>
+                      key !== "image" &&
+                      value !== null && <td key={key}>{value}</td>
+                  )}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
-    <div className="product">
-      <div className="product-image">
-        <img
-          src={require(`../../images/${product.image}`)}
-          alt={product.name}
-        />
+    <div className="Singleproduct">
+      <div className="Singleproduct-info">
+        <div className="Singleproduct-image">
+          <img
+            src={require(`../../images/${product.image}`)}
+            alt={product.name}
+          />
+        </div>
+        <div className="Singleproduct-details">
+          <h3>{product.name}</h3>
+          <h4>{product.price}</h4>
+          <p>Description: {product.description}</p>
+        </div>
       </div>
-      <h3>{product.name}</h3>
-      <h4>{product.price}</h4>
-      <p>Description: {product.description}</p>
+      {renderMeasurements()}
+      {renderOptions()}
       <button onClick={handleAddToCart}>Add to Cart</button>
     </div>
   );
