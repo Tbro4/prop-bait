6ac6df5c22180e2ca53cff83d4247876a776bfd9
Tyler Brooks <thetylerbrooks@gmail.com>
2023-06-19 17:41:54 -0600

    cleaned up some unused code

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -96,20 +96,6 @@ export const QUERY_PRODUCTS_BY_KEYWORD = gql`
   }
 `;
 
-export const QUERY_ALL_PRODUCTS = gql`
-  query Query {
-    products {
-      _id
-      name
-      price
-      description
-      brand
-      category
-      subCategory
-    }
-  }
-`;
-
 export const QUERY_USER = gql`
   query user($username: String!) {
     user(username: $username) {
@@ -119,50 +105,6 @@ export const QUERY_USER = gql`
       thoughts {
         _id
         thoughtText
-        createdAt
-      }
-    }
-  }
-`;
-
-export const QUERY_THOUGHTS = gql`
-  query getThoughts {
-    thoughts {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-    }
-  }
-`;
-
-export const QUERY_SINGLE_THOUGHT = gql`
-  query getSingleThought($thoughtId: ID!) {
-    thought(thoughtId: $thoughtId) {
-      _id
-      thoughtText
-      thoughtAuthor
-      createdAt
-      comments {
-        _id
-        commentText
-        commentAuthor
-        createdAt
-      }
-    }
-  }
-`;
-
-export const QUERY_ME = gql`
-  query me {
-    me {
-      _id
-      username
-      email
-      thoughts {
-        _id
-        thoughtText
-        thoughtAuthor
         createdAt
       }
     }
