0f565f9b9140db40288578b56987b2f9680945b0
Tyler Brooks <thetylerbrooks@gmail.com>
2023-06-30 16:35:58 -0600

    Cart-itmes stay inline on large screen, but wrap oddly on smaller screens. Needs more styling, but this is a good save point?

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_USER_CART } from "../../utils/queries";
 import AuthService from "../../utils/auth";
@@ -6,12 +6,30 @@ import "./Cart.css";
 
 const Cart = () => {
   const profile = AuthService.getProfile();
-  console.log(profile.data);
   const userId = profile ? profile.data._id : null;
 
   const { loading, error, data } = useQuery(QUERY_USER_CART, {
     variables: { userId },
   });
+
+  const cartContainerRef = useRef(null);
+
+  useEffect(() => {
+    if (cartContainerRef.current) {
+      const cartItems =
+        cartContainerRef.current.getElementsByClassName("cart-item-details");
+      let maxWidth = 0;
+
+      for (let i = 0; i < cartItems.length; i++) {
+        const width = cartItems[i].offsetWidth;
+        maxWidth = Math.max(maxWidth, width);
+      }
+
+      for (let i = 0; i < cartItems.length; i++) {
+        cartItems[i].style.width = `${maxWidth}px`;
+      }
+    }
+  }, [data]);
 
   if (loading) {
     return <p>Loading cart data...</p>;
@@ -21,52 +39,51 @@ const Cart = () => {
     return <p>Error retrieving cart data.</p>;
   }
 
-  console.log(data);
-
   const userCart = data.userCart;
 
   return (
-    <div className="App">
+    <div className="cart-container" ref={cartContainerRef}>
       <br />
-      <br />
-      <br />
-      <br />
-      <h1>{profile.data.username}'s cart </h1>
+      <h1>{profile.data.username}'s cart</h1>
 
       {userCart.map((item) => (
-        <div key={item._id}>
+        <div key={item._id} className="cart-item">
           {item.option && item.option.image ? (
             <img
-              className="option-image"
+              className="option-image cart-image"
               src={require(`../../images/${item.option.image}`)}
               alt="Product"
             />
           ) : (
             <img
-              className="option-image"
+              className="option-image cart-image"
               src={require(`../../images/${item.product.image}`)}
               alt="Product"
             />
           )}
-          <p>{item.product.name}</p>
-          {item.option &&
-            Object.entries(item.option).map(([key, value]) => {
-              if (
-                value &&
-                key !== "__typename" &&
-                key !== "_id" &&
-                key !== "image"
-              ) {
-                return (
-                  <p key={key}>
-                    {key}: {value}
-                  </p>
-                );
-              }
-              return null;
-            })}
-          <p>Price: {item.product.price}</p>
-          <p>Quantity: {item.quantity}</p>
+          <div className="cart-item-details">
+            <p className="cart-item-name">{item.product.name}</p>
+            {item.option &&
+              Object.entries(item.option).map(([key, value]) => {
+                if (
+                  value &&
+                  key !== "__typename" &&
+                  key !== "_id" &&
+                  key !== "image"
+                ) {
+                  return (
+                    <p key={key} className="cart-item-options">
+                      {key}: {value}
+                    </p>
+                  );
+                }
+                return null;
+              })}
+          </div>
+          <div className="cart-qty-price">
+            <p className="cart-item-quantity">Quantity: {item.quantity}</p>
+            <p className="cart-item-price">${item.product.price}</p>
+          </div>
         </div>
       ))}
     </div>
