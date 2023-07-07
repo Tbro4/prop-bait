32117cc0e311b3209cefbd3e21a80fdddad65cf0
Tyler Brooks <thetylerbrooks@gmail.com>
2023-07-06 18:22:19 -0600

    products can be sorted 3 ways and filtered by brand and category

--- client/src/components/SubCategories/SubCategories.js
+++ client/src/components/SubCategories/SubCategories.js
@@ -25,6 +25,8 @@ const SubCategories = ({ category, onSubCategoryClick, onProductClick }) => {
   });
   const [isFilterOpen, setIsFilterOpen] = useState(false);
   const [selectedBrands, setSelectedBrands] = useState([]);
+  const [selectedSubCategories, setSelectedSubCategories] = useState([]);
+  const [selectedSortOption, setSelectedSortOption] = useState("");
 
   const handleFilterToggle = () => {
     setIsFilterOpen(!isFilterOpen);
@@ -45,6 +47,10 @@ const SubCategories = ({ category, onSubCategoryClick, onProductClick }) => {
 
   const subCategories = subCategoryData?.subCategoryByCategory || [];
   const products = productsData?.productsByCategory || [];
+  // console.log(products);
+  const uniqueSubCategories = [
+    ...new Set(products.map((product) => product.subCategory)),
+  ];
   const uniqueBrands = [...new Set(products.map((product) => product.brand))];
 
   const handleSubCategoryClick = (subCategory) => {
@@ -66,10 +72,46 @@ const SubCategories = ({ category, onSubCategoryClick, onProductClick }) => {
     }
   };
 
-  const filteredProducts =
-    selectedBrands.length > 0
-      ? products.filter((product) => selectedBrands.includes(product.brand))
-      : products;
+  const handleSubCategoryChange = (e) => {
+    const subCategory = e.target.value;
+    if (selectedSubCategories.includes(subCategory)) {
+      setSelectedSubCategories(
+        selectedSubCategories.filter(
+          (selectedSubCategory) => selectedSubCategory !== subCategory
+        )
+      );
+    } else {
+      setSelectedSubCategories([...selectedSubCategories, subCategory]);
+    }
+  };
+
+  const handleSortChange = (e) => {
+    setSelectedSortOption(e.target.value);
+  };
+
+  //we map over filteredProducts in the return to display the filtered products
+  const filteredProducts = products.filter((product) => {
+    if (selectedBrands.length > 0 && !selectedBrands.includes(product.brand)) {
+      return false; // Skip if the brand doesn't match the selected brands
+    }
+    if (
+      selectedSubCategories.length > 0 &&
+      !selectedSubCategories.includes(product.subCategory)
+    ) {
+      return false; // Skip if the subcategory doesn't match the selected subcategories
+    }
+    return true; // Include the product in the filtered list
+  });
+
+  let sortedProducts = [...filteredProducts];
+
+  if (selectedSortOption === "price-desc") {
+    sortedProducts.sort((a, b) => b.price - a.price); // Sort by price high to low
+  } else if (selectedSortOption === "price-asc") {
+    sortedProducts.sort((a, b) => a.price - b.price); // Sort by price low to high
+  } else if (selectedSortOption === "name-asc") {
+    sortedProducts.sort((a, b) => a.name.localeCompare(b.name)); // Sort by name A-Z
+  }
 
   return (
     <div className="sub-cat">
@@ -82,17 +124,22 @@ const SubCategories = ({ category, onSubCategoryClick, onProductClick }) => {
             <h3>{subCategory.subCategory}</h3>
           </div>
         ))}
-        {/* Button to toggle filter */}
-        <Button onClick={handleFilterToggle}>Toggle Filter</Button>
       </div>
+      {/* Button to toggle filter */}
+      <Button onClick={handleFilterToggle}>Toggle Filter</Button>
       <Drawer anchor="left" open={isFilterOpen} onClose={handleFilterToggle}>
         <div className="sort-filter">
-          <select id="sortBy" value="">
+          <select
+            id="sortBy"
+            value={selectedSortOption}
+            onChange={handleSortChange}
+          >
             <option value="" disabled hidden>
               Sort
             </option>
-            <option value="price">Price</option>
-            <option value="az">A-Z</option>
+            <option value="price-desc">Price High to Low</option>
+            <option value="price-asc">Price Low to High</option>
+            <option value="name-asc">A-Z</option>
           </select>
 
           <div className="filter">
@@ -108,29 +155,45 @@ const SubCategories = ({ category, onSubCategoryClick, onProductClick }) => {
                 {brand}
               </label>
             ))}
+            <h3>-Category</h3>
+            {uniqueSubCategories.map((subcategory) => (
+              <label key={subcategory}>
+                <input
+                  type="checkbox"
+                  value={subcategory}
+                  checked={selectedSubCategories.includes(subcategory)}
+                  onChange={handleSubCategoryChange}
+                />
+                {subcategory}
+              </label>
+            ))}
           </div>
         </div>
       </Drawer>
       <h1>{subCategories[0].category}</h1>
       <div className="products">
-        {filteredProducts.map((product) => (
-          <div
-            key={product._id}
-            className="product"
-            onClick={() => handleProductClick(product._id)}
-          >
-            <div className="product-image">
-              <img
-                src={require(`../../images/${product.image}`)}
-                alt={product.product}
-              />
+        {filteredProducts.length === 0 ? (
+          <div>No options available with those filters chosen</div>
+        ) : (
+          sortedProducts.map((product) => (
+            <div
+              key={product._id}
+              className="product"
+              onClick={() => handleProductClick(product._id)}
+            >
+              <div className="product-image">
+                <img
+                  src={require(`../../images/${product.image}`)}
+                  alt={product.product}
+                />
+              </div>
+              <div className="product-info">
+                <h3>{product.name}</h3>
+                <h4>{product.price}</h4>
+              </div>
             </div>
-            <div className="product-info">
-              <h3>{product.name}</h3>
-              <h4>{product.price}</h4>
-            </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
